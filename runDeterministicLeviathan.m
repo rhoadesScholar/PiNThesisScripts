186a97846724bc8a2c9b657cef7f40477cae5015
Jeff Rhoades <37990507+rhoadesScholar@users.noreply.github.com>
2019-08-13 12:28:19 -0400

    made function of run script

---
+++ runDeterministicLeviathan.m
@@ -0,0 +1,48 @@
+function data = runDeterministicLeviathan(N, M)
+    if nargin < 2
+        M = [2, 7];
+    end
+    if nargin < 1
+        N = 10;
+    end
+    Y = randperm(N);
+    Z = randperm(N);
+    Obs = cell(length(M)+1, length(Y)*length(Z));
+
+    %% 1:1 pairing
+    X = randperm(length(Y)*length(Z));
+    k = 1;
+    for i = 1:length(Y)
+        for j = 1:length(Z)
+            obs(k, :) = split(num2str([X(k), Y(i), Z(j)]))';
+            k = k + 1;
+        end
+    end
+    % obs(:, 1) = split(num2str(Y));
+    % obs(:, 1) = split(num2str(Z));
+    Obs(1, 1:size(obs, 1), 1:size(obs,2)) = obs;
+    labelStr{1} = '1:1';
+
+    %% modulo M data structure
+    mInd = 0;
+    for m = M
+        P_a = zeros(m, length(Y), length(Z));
+        X = cell2mat(arrayfun(@(y) mod(nansum([ones(size(Y))*y; Z], 1), m)+1, Y, 'UniformOutput', false));%mod(nansum([Y; Z], 1), m)+1;
+        obs = cell(length(X), 3);
+        k = 1;
+        for i = 1:length(Y)
+            for j = 1:length(Z)
+                P_a(X(k), Y(i), Z(j)) = 1;
+                obs(k, :) = split(num2str([X(k), Y(i), Z(j)]))';
+                k = k + 1;
+            end
+        end
+        Obs(mInd + 1, 1:size(obs, 1), 1:size(obs,2)) = obs;
+        mInd = mInd + 1;
+        labelStr{mInd+1} = ['Mod' num2str(m)];
+    end
+
+    %% run it
+
+    data = showLearning(Obs, {'1:1' 'Mod2' 'Mod7'}, true);
+end
