a3721c60d7e9a71aba7a709ee04d530ad5a7e136
Jeff Rhoades <37990507+rhoadesScholar@users.noreply.github.com>
2020-01-29 14:58:09 -0500

    batch working

--- JanRotation/compareSims.jl
+++ JanRotation/compareSims.jl
@@ -2,19 +2,36 @@ include("runBatchSim.jl")
 include("linspecer.jl")
 using Printf
 
-function compareSims(flexVarName, flexVarValues)
-    staticVarStr = Meta.parse("")
+dt = .1
+A = [1 0 0 dt;
+     0 1 dt -dt;
+     0 0 1 0;
+     0 0 0 1]
+C = [0. 1. 0 0;
+     0 0 0 1]
+muPrior = [0. 0.;
+           10. 10.;
+           0. 0.;
+           1. 0.]
+endT = 100
+sigmas = [1., 100, 50, 1]
 
-    variationNum = length(flexVarValues)
+static = StaticWorld(A, C, muPrior, endT, dt)
+simBase = SimOpts(sigmas)
+
+flexSigValues = [.01, .1, 1, 10, 100]
+flexSigInd = 4
+flexVarName = "sigmaVest"
+
+function compareSims(static::StaticWorld, simBase::SimOpts, flexSigValues::Array{Float64,1}, flexSigInd::Int64, flexVarName::String)
+    variationNum = length(flexSigValues)
     colors = linspecer(variationNum)
     figure()
     for v = 1:variationNum
-        thisFlexArg = Meta.parse(@sprintf("%s=%s", flexVarName, string.(flexVarValues[v])))
-        colorArg = :(color = $(colors[v,:]))
-        labelArg = Meta.parse(@sprintf("label = \"%s = %s\"", flexVarName, string.(flexVarValues[v])))
-        argS = [thisFlexArg colorArg labelArg]
-        # println(argS)
-        runBatchSim(argS)
+        println(string("Variation #", v))
+        plotOpts = PlotOpts(@sprintf("%s = %s", flexVarName, string.(flexSigValues[v])), colors[v,:])
+        simOpts = SimOpts(simBase, flexSigInd, flexSigValues[v])
+        runBatchSim(plotOpts, static, simOpts)
     end
 
     legend()
--- JanRotation/runBatchSim.jl
+++ JanRotation/runBatchSim.jl
@@ -4,102 +4,127 @@ using Random
 using LinearAlgebra
 using Statistics
 
+struct StaticWorld
+    #Sim Priors [position, object distance, object motion, velocity]
+    A::Array{Float64,2}
+    C::Array{Float64,2}
+    muPrior::Array{Float64,2}
+    endT::Number
+    dt::Number
+    endI::Int
+end
+StaticWorld(A::Array{Float64,2}, C::Array{Float64,2}, muPrior::Array{Float64,2}, endT::Number, dt::Number) =
+    StaticWorld(A::Array{Float64,2}, C::Array{Float64,2}, muPrior::Array{Float64,2}, endT::Number, dt::Number, Integer(ceil(endT/dt)))
+StaticWorld(A::Array{Float64,2}, C::Array{Float64,2}, muPrior::Array{Float64,2}) =
+    StaticWorld(A::Array{Float64,2}, C::Array{Float64,2}, muPrior::Array{Float64,2}, 500, .5, 1000)
 
-function runSim(muInit, A, C, oldMu, oldVar, sigmaEye, sigmaVest, a, inNoise, endI, sigmaExpected)
-    err = zeros(size(muInit,1),endI)*NaN
-    oldZ = muInit + [sqrt(a*sigmaEye)*[randn() randn()]; sqrt(a/(100*eps()))*[randn() randn()]; sqrt(a*sigmaVest)*[randn() randn()]]
-    for i = 1:endI
-        #Get actual latent
-        oldZ = A*oldZ
+struct SimOpts
+    sigmas::Array{Float64,1}
+    a::Float64
+    N::Int64
+end
+SimOpts(num::Int64) = SimOpts(ones(num,1))
+SimOpts(static::StaticWorld) = SimOpts(ones(size(static.A,1)))
+SimOpts(sigmas::Array{Float64,1}) = SimOpts(sigmas::Array{Float64,1}, 1, 1000)
+SimOpts(base::SimOpts, ind::Int64, sig::Number) = SimOpts(setindex!(base.sigmas, sig, ind), base.a, base.N)
+
+struct PlotOpts
+    label::String
+    color::Array{Float64,1}
+    width::Float64
+    alpha::Float64
+end
+PlotOpts(label::String) = PlotOpts(label::String, [0,0,1], 2, .3)
+PlotOpts(label::String, color::Array{Float64,1}) = PlotOpts(label::String, color::Array{Float64,1}, 2, .3)
+
+struct InitWorld
+    Z::Array{Float64,2}
+    Var::Array{Float64,2}
+    sigmaPrior::Array{Float64,2}
+end
+InitWorld(static::StaticWorld, simOpts::SimOpts) = InitWorld(
+    static.muPrior + vcat([sqrt(simOpts.a*s)*randn(size(static.muPrior,2)) for s in simOpts.sigmas]'...),
+    diagm(simOpts.a*simOpts.sigmas),
+    static.C*(diagm(simOpts.a*simOpts.sigmas))*static.C'/static.dt)
+
+struct FullWorld
+    Zs::Array{Array{Float64,2},1}
+    initVar::Array{Float64,2}
+    sigmaPrior::Array{Float64,2}
+    noise
+    A::Array{Float64,2}
+    C::Array{Float64,2}
+    muPrior::Array{Float64,2}
+    endT::Float64
+    dt::Float64
+    endI::Int128
+end
+FullWorld(static::StaticWorld, init::InitWorld) =
+    FullWorld([(static.A^t)*init.Z for t in 0:static.endI], init.Var, init.sigmaPrior,
+    MvNormal(zeros(size(init.sigmaPrior,1)), init.sigmaPrior),
+    static.A, static.C, static.muPrior, static.endT, static.dt, static.endI)
+FullWorld(static::StaticWorld, simOpts::SimOpts) =
+    FullWorld(static::StaticWorld, InitWorld(static::StaticWorld, simOpts::SimOpts))
+
+function runSim(kworld::FullWorld)
+    err = zeros(size(kworld.muPrior,1),kworld.endI+1)*NaN
+    oldVar = kworld.initVar
+    oldMu = kworld.muPrior
+    err[:,1] = sum((oldMu - kworld.Zs[1]).^2,dims=2)
+    for i = 2:kworld.endI+1
         #Get observations
-
-        Y = C*oldZ + rand!(inNoise,zeros(size(C*oldZ)))
+        Y = C*kworld.Zs[i] + rand!(kworld.noise,zeros(size(C*kworld.Zs[i])))
 
         #Get filter()
-        K = A*oldVar*A'*C'/(C*A*oldVar*A'*C' + sigmaExpected)
-        oldVar = (I - K*C)*A*oldVar*A'
-        oldMu = A*oldMu + K*(Y-C*A*oldMu);
-        err[:,i] = sum((oldMu - oldZ).^2,dims=2)
+        K = kworld.A*oldVar*kworld.A'*kworld.C'/(kworld.C*kworld.A*oldVar*kworld.A'*kworld.C' + kworld.sigmaPrior)
+        oldVar = (I - K*kworld.C)*kworld.A*oldVar*kworld.A'
+        oldMu = kworld.A*oldMu + K*(Y-kworld.C*kworld.A*oldMu);
+        err[:,i] = sum((oldMu - kworld.Zs[i]).^2,dims=2)
     end
     return err
 end
 
-function plotshade(y, err, x, alpha, color, width, label)
-    f = fill_between(x[1:length(y)], y+err, y-err,color=color, alpha=alpha)
+function plotshade(y, err, x, opts::PlotOpts)
+    f = fill_between(x[1:length(y)], y+err, y-err,color=opts.color, alpha=opts.alpha)
 
     # f.Annotation.LegendInformation.IconDisplayStyle = "off"
-    p = plot(x[1:length(y)],y,color=color[:],linewidth = width, label=label) ## change color | linewidth to adjust mean line()
+    p = plot(x[1:length(y)],y,color=opts.color[:],linewidth = opts.width, label=opts.label) ## change color | linewidth to adjust mean line()
     # @show p
 end
 
-function plotRMSE(RMSE, sems, dt, color, label)
+function plotRMSE(RMSE, sems, dt, opts::PlotOpts)
     endI = size(RMSE,2)-1
-    # gcf()
+
     subplot(2, 2, 1)
-    plotshade(RMSE[1,:], sems[1,:], 0:dt:endI*dt, .3, color, 2, label)
+    plotshade(RMSE[1,:], sems[1,:], 0:dt:endI*dt, opts)
     xlabel("time")
     ylabel("root mean square error")
     title("Position RMSE")
 
     subplot(2, 2, 3)
-    plotshade(RMSE[2,:], sems[2,:], 0:dt:endI*dt, .3, color, 2, label)
+    plotshade(RMSE[2,:], sems[2,:], 0:dt:endI*dt, opts)
     xlabel("time")
     ylabel("root mean square error")
     title("Object Distance RMSE")
 
     subplot(2, 2, 4)
-    plotshade(RMSE[3,:], sems[3,:], 0:dt:endI*dt, .3, color, 2, label)
+    plotshade(RMSE[4,:], sems[4,:], 0:dt:endI*dt, opts)
     xlabel("time")
     ylabel("root mean square error")
     title("Velocity RMSE")
     return
 end
 
-function runBatchSim(argS)
-    # pyplot()
+function runBatchSim(plotOpts::PlotOpts, static::StaticWorld, simOpts::SimOpts)
+    kworld = FullWorld(static, simOpts)
 
-    #Batch Settings
-    N = 1000
-
-    #Sim Priors [position, object distance, velocity]
-    P = [0 0]
-    D = [10 10]
-    V = [1 0]
-
-    #Sim Settings
-    a = 1
-    endT = 100
-    dt = .5
-    sigmaVest = 1
-    sigmaEye = 1
-
-    #Plot Serrings
-
-    # try
-        [eval(argS[v]) for v in 1:length(argS)]
-    # catch
-        # println("No argS supplied.")
-        # color = [0 0 1]
-    # end
-
-    sigmaExpected = [sigmaEye 0; 0 sigmaVest]/dt
-    endI = Integer(ceil(endT/dt))
-
-    A = [1 0 dt; 0 1 -dt; 0 0 1]
-    C = [0 1 0; 0 0 1]
-    muInit = [P; D; V;]
-
-    inNoise = MvNormal([0, 0], sigmaExpected)
-
-    errs = zeros(size(muInit,1),endI,N)*NaN
-    for n = 1:N#SHOULD BE PARFOR [if could start pool correctly...]
-        oldMu = muInit
-        oldVar = [a*sigmaEye 0 0; 0 a/(100*eps()) 0; 0 0 a*sigmaVest]
-        errs[:,:,n] = runSim(muInit, A, C, oldMu, oldVar, sigmaEye, sigmaVest, a, inNoise, endI, sigmaExpected)
+    errs = zeros(size(static.muPrior,1),static.endI+1,simOpts.N)*NaN
+    for n = 1:simOpts.N#SHOULD BE PARFOR [if could start pool correctly...]
+        errs[:,:,n] = runSim(kworld)
     end
     RMSE = sqrt.(dropdims(mean(errs, dims=3), dims=3))
-    sems = dropdims(std(errs;mean=RMSE,dims=3)/sqrt(N), dims=3)
-    plotRMSE(RMSE, sems, dt, color, label)
+    sems = dropdims(std(errs;mean=RMSE,dims=3)/sqrt(simOpts.N), dims=3)
+    plotRMSE(RMSE, sems, static.dt, plotOpts)
 
     return errs
 end
